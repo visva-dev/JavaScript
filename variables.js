e9f345b826c4f81b9e0b40258fec373f2c61010c
Visvaldas <visva.rapalis@gmail.com>
2020-10-27 11:01:17 +0200

    Add variables examples

---
+++ variables.js
@@ -0,0 +1,19 @@
+let name = 'Visvaldas';
+
+// Case matters
+let Rules = 'rules';
+// is different
+let rules = 'rules'; 
+
+// These naming ar good too
+let $rules = 'something'
+let _rules = 'something'
+let $ru_les = 'something'
+let myAge30 = 'something'
+
+// This is bad
+// let 12name = 'sdfsdfsd'
+// let =rules = 'something'
+
+let itCanChange = "let is used for variables which can be changed"
+const itCannotBeChanged = "const is used for things you don't want to change"
