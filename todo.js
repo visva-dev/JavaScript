a0014d6db037ef4172c3d0d66297748bf5efcafb
Visvaldas <visva.rapalis@gmail.com>
2020-10-28 22:13:39 +0200

    Add deletion items functionality

--- todo.js
+++ todo.js
@@ -1,9 +1,10 @@
 // Selectors
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list'); 
+const todoList = document.querySelector('.todo-list');
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 // Functions
 function addTodo(event) {
   // prevent form from submitting
@@ -21,13 +22,28 @@ function addTodo(event) {
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
   completedButton.classList.add('complete-btn');
   todoDiv.appendChild(completedButton);
-    // Check Trash Button
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add('trash-btn');
-    todoDiv.appendChild(trashButton);
-    // Append To List
-    todoList.appendChild(todoDiv);
-    // Crear todo input value after add
-    todoInput.value = '';
+  // Check Trash Button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+  // Append To List
+  todoList.appendChild(todoDiv);
+  // Crear todo input value after add
+  todoInput.value = '';
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  // Delete todo
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+
+  // Check mark
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
 }
