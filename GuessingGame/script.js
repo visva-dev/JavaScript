88d885bf374b0efda7f02458c39a6e0570b12af2
Visvaldas <visva.rapalis@gmail.com>
2020-11-10 16:24:39 +0200

    Add game logic

--- GuessingGame/script.js
+++ GuessingGame/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Select element
 // console.log(document.querySelector('.message'));
 
@@ -13,3 +13,38 @@ document.querySelector('.score').textContent = 19;
 
 document.querySelector('.guess').value = 2;
 console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Uou Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Uou Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
