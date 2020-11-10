35262dd013e51f0a1e10da6e2e17a36f23c6e310
Visvaldas <visva.rapalis@gmail.com>
2020-11-10 17:06:14 +0200

    Add HighScore counter

--- GuessingGame/script.js
+++ GuessingGame/script.js
@@ -19,6 +19,7 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
+let highScore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -32,6 +33,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     // --------------
+
+    // Set high score
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    // --------------
+
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High!';
@@ -53,7 +62,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 // Reset the game button
-document.querySelector('.again').addEventListener ('click', function() {
+document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.message').textContent = 'Start Guessing...!';
@@ -62,4 +71,4 @@ document.querySelector('.again').addEventListener ('click', function() {
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-})
+});
