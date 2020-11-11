82176ab44639b8f998678e7e01e25d1b019f12fb
Visvaldas <visva.rapalis@gmail.com>
2020-11-11 16:10:23 +0200

    Add selectors

--- PopUpExample/script.js
+++ PopUpExample/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// Selects all items
+const btnOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+
+// Select each item
+for (let i = 0; i < btnOpenModal.length; i++)
+  console.log(btnOpenModal[i].textContent);
